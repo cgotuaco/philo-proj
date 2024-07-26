b04b91f8f0e106d018a47b27ca355380b1934f86
casey :) <casey@gotuaco.com>
2024-07-26 21:27:29 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+/* Place your JavaScript in this file */                                            
+document.getElementById('h3').onmouseover = function() {
+  this.style.color = 'red';
+
+
+  var header = document.getElementById('header');
